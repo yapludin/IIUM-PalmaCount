a72ea30714e088f091062f82d9d03b4119d51020
Isyraf <isyrafzulkefli@gmail.com>
2026-01-05 01:08:18 +0800

    backend initials

--- palma-v2/palma-frontend/app.py
+++ palma-v2/palma-frontend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask_cors import CORS
 from flask_login import login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
